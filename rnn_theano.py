0c3409e066595620a24ef48313ba0c383fa06429
asmitd <asmitd@vmware.com>
2017-03-02 12:08:12 +0530

    rnn generative model

---
+++ rnn_theano.py
@@ -0,0 +1,165 @@
+import numpy as np
+import theano as theano
+import theano.tensor as T
+import operator
+import nltk
+import urllib
+import csv
+import itertools
+import numpy as np
+
+class RNNTheano:
+
+    def __init__(self, word_dim, hidden_dim=100, bptt_truncate=4):
+        # Assign instance variables
+        self.word_dim = word_dim
+        self.hidden_dim = hidden_dim
+        self.bptt_truncate = bptt_truncate
+        # Randomly initialize the network parameters
+        U = np.random.uniform(-np.sqrt(1./word_dim), np.sqrt(1./word_dim), (hidden_dim, word_dim))
+        V = np.random.uniform(-np.sqrt(1./hidden_dim), np.sqrt(1./hidden_dim), (word_dim, hidden_dim))
+        W = np.random.uniform(-np.sqrt(1./hidden_dim), np.sqrt(1./hidden_dim), (hidden_dim, hidden_dim))
+        # Theano: Created shared variables
+        self.U = theano.shared(name='U', value=U.astype(theano.config.floatX))
+        self.V = theano.shared(name='V', value=V.astype(theano.config.floatX))
+        self.W = theano.shared(name='W', value=W.astype(theano.config.floatX))
+        # We store the Theano graph here
+        self.theano = {}
+        self.__theano_build__()
+
+    def __theano_build__(self):
+        U, V, W = self.U, self.V, self.W
+        x = T.ivector('x')
+        y = T.ivector('y')
+        def forward_prop_step(x_t, s_t_prev, U, V, W):
+            s_t = T.tanh(U[:,x_t] + W.dot(s_t_prev))
+            o_t = T.nnet.softmax(V.dot(s_t))
+            return [o_t[0], s_t]
+        [o,s], updates = theano.scan(
+            forward_prop_step,
+            sequences=x,
+            outputs_info=[None, dict(initial=T.zeros(self.hidden_dim))],
+            non_sequences=[U, V, W],
+            truncate_gradient=self.bptt_truncate,
+            strict=True)
+
+        prediction = T.argmax(o, axis=1)
+        o_error = T.sum(T.nnet.categorical_crossentropy(o, y))
+
+        # Gradients
+        dU = T.grad(o_error, U)
+        dV = T.grad(o_error, V)
+        dW = T.grad(o_error, W)
+
+        # Assign functions
+        self.forward_propagation = theano.function([x], o)
+        self.predict = theano.function([x], prediction)
+        self.ce_error = theano.function([x, y], o_error)
+        self.bptt = theano.function([x, y], [dU, dV, dW])
+
+        # SGD
+        learning_rate = T.scalar('learning_rate')
+        self.sgd_step = theano.function([x,y,learning_rate], [],
+                                        updates=[(self.U, self.U - learning_rate * dU),
+                                                 (self.V, self.V - learning_rate * dV),
+                                                 (self.W, self.W - learning_rate * dW)])
+
+    def calculate_total_loss(self, X, Y):
+        return np.sum([self.ce_error(x,y) for x,y in zip(X,Y)])
+
+    def calculate_loss(self, X, Y):
+        # Divide calculate_loss by the number of words
+        num_words = np.sum([len(y) for y in Y])
+        return self.calculate_total_loss(X,Y)/float(num_words)
+
+def softmax(x):
+    xt = np.exp(x - np.max(x))
+    return xt / np.sum(xt)
+
+def save_model_parameters_theano(outfile, model):
+    U, V, W = model.U.get_value(), model.V.get_value(), model.W.get_value()
+    np.savez(outfile, U=U, V=V, W=W)
+    print "Saved model parameters to %s." % outfile
+
+def load_model_parameters_theano(path, model):
+    npzfile = np.load(path)
+    U, V, W = npzfile["U"], npzfile["V"], npzfile["W"]
+    model.hidden_dim = U.shape[0]
+    model.word_dim = U.shape[1]
+    model.U.set_value(U)
+    model.V.set_value(V)
+    model.W.set_value(W)
+    print "Loaded model parameters from %s. hidden_dim=%d word_dim=%d" % (path, U.shape[0], U.shape[1])
+
+def generate_sentence(model):
+    # We start the sentence with the start token
+    new_sentence = [word_to_index[sentence_start_token]]
+    # Repeat until we get an end token
+    while not new_sentence[-1] == word_to_index[sentence_end_token]:
+        next_word_probs = model.forward_propagation(new_sentence)
+        sampled_word = word_to_index[unknown_token]
+        # We don't want to sample unknown words
+        while sampled_word == word_to_index[unknown_token]:
+            samples = np.random.multinomial(1, next_word_probs[-1])
+            sampled_word = np.argmax(samples)
+        new_sentence.append(sampled_word)
+    sentence_str = [index_to_word[x] for x in new_sentence[1:-1]]
+    return sentence_str
+
+vocabulary_size = 8000
+unknown_token = "UNKNOWN_TOKEN"
+sentence_start_token = "SENTENCE_START"
+sentence_end_token = "SENTENCE_END"
+
+# Read the data and append SENTENCE_START and SENTENCE_END tokens
+print "Reading CSV file..."
+f = urllib.urlopen('https://raw.githubusercontent.com/dennybritz/rnn-tutorial-rnnlm/master/data/reddit-comments-2015-08.csv')
+reader = csv.reader(f, skipinitialspace=True)
+reader.next()
+# Split full comments into sentences
+sentences = itertools.chain(*[nltk.sent_tokenize(x[0].decode('utf-8').lower()) for x in reader])
+# Append SENTENCE_START and SENTENCE_END
+sentences = ["%s %s %s" % (sentence_start_token, x, sentence_end_token) for x in sentences]
+print "Parsed %d sentences." % (len(sentences))
+
+# Tokenize the sentences into words
+tokenized_sentences = [nltk.word_tokenize(sent) for sent in sentences]
+
+# Count the word frequencies
+word_freq = nltk.FreqDist(itertools.chain(*tokenized_sentences))
+print "Found %d unique words tokens." % len(word_freq.items())
+
+# Get the most common words and build index_to_word and word_to_index vectors
+vocab = word_freq.most_common(vocabulary_size-1)
+index_to_word = [x[0] for x in vocab]
+index_to_word.append(unknown_token)
+word_to_index = dict([(w,i) for i,w in enumerate(index_to_word)])
+
+print "Using vocabulary size %d." % vocabulary_size
+print "The least frequent word in our vocabulary is '%s' and appeared %d times." % (vocab[-1][0], vocab[-1][1])
+
+# Replace all words not in our vocabulary with the unknown token
+for i, sent in enumerate(tokenized_sentences):
+    tokenized_sentences[i] = [w if w in word_to_index else unknown_token for w in sent]
+
+print "\nExample sentence: '%s'" % sentences[0]
+print "\nExample sentence after Pre-processing: '%s'" % tokenized_sentences[0]
+
+# Create the training data
+X_train = np.asarray([[word_to_index[w] for w in sent[:-1]] for sent in tokenized_sentences])
+y_train = np.asarray([[word_to_index[w] for w in sent[1:]] for sent in tokenized_sentences])
+
+num_sentences = 10
+senten_min_length = 7
+
+model = RNNTheano(vocabulary_size, hidden_dim=50)
+# losses = train_with_sgd(model, X_train, y_train, nepoch=50)
+# save_model_parameters_theano('./data/trained-model-theano.npz', model)
+load_model_parameters_theano('trained-model-theano.npz', model)
+
+for i in range(num_sentences):
+    sent = []
+    # We want long sentences, not sentences with one or two words
+    while len(sent) < senten_min_length:
+        sent = generate_sentence(model)
+    print " ".join(sent)
